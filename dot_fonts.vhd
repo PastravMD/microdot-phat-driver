24ba9c5e5cd249a44101da8c5a25a2863dbfd2d9
PastravMD <mihai_dragan@outlook.com>
2018-06-23 01:25:32 +0100

    fix messy merge of dot_fonts.vhd

--- dot_fonts.vhd
+++ dot_fonts.vhd
@@ -8,12 +8,14 @@ use ieee.numeric_std.all;
 
 -- TODO: add support for decimal dot
 package dot_fonts is
-	type dot_char_t is 
-		array (6 downto 0, 4 downto 0) of std_logic;
+	type dot_matrix_t is array (6 downto 0, 4 downto 0) of std_logic;
 
-	function get_dot_char(ascii_code : integer) return dot_char_t;
-	function col(x : integer; dots : dot_char_t) return std_logic_vector;
-	function row(y : integer; dots : dot_char_t) return std_logic_vector;
+	function get_dot_char(ascii_code:natural) return dot_matrix_t;
+	function col(x : natural; dots: dot_matrix_t) return std_logic_vector;
+	function row(y : natural; dots: dot_matrix_t) return std_logic_vector;
+	function get_char_line(dots:		dot_matrix_t;
+			       line_nr:		natural;
+			       module_sel:	std_logic) return std_logic_vector;
 end package dot_fonts;
 
 library ieee;
@@ -27,45 +29,6 @@ package body dot_fonts is
 		variable dot_char : dot_matrix_t;
 	begin
 	case ascii_code is
-		when 32   => dot_char := (('0','0','1','0','0'),
-					  ('0','0','1','0','0'),
-					  ('0','0','1','0','0'),
-					  ('0','0','1','0','0'),
-					  ('0','0','1','0','0'),
-					  ('0','0','1','0','0'),
-					  ('0','0','1','0','0'));
-
-		when others => dot_char := (('0','1','0','1','0'),
-					    ('0','1','0','1','0'),
-					    ('0','1','0','1','0'),
-					    ('0','1','0','1','0'),
-					    ('0','1','0','1','0'),
-					    ('0','1','0','1','0'),
-					    ('0','1','0','1','0'));
-	end case;
-	return dot_char;
-	end function get_dot_char;
-
-	function col(x : integer; dots : dot_char_t) return std_logic_vector is
-	begin
-		return '0' & dots(6, x) & dots(5, x) & dots(4, x) & dots(3, x) & 
-			dots(2, x) & dots(1, x) & dots(0, x);
-	end function col;
-
-	function row(y : integer; dots : dot_char_t) return std_logic_vector is
-	begin
-		return "000" & dots(y, 4) & dots(y, 3) & dots(y, 2) & dots(y, 1) & dots(y, 0);
-	end function row;
-
-end package body dot_fonts;
-
-	type dot_matrix_t is array (6 downto 0, 4 downto 0) of std_logic;
-	function get_dot_char(ascii_code:natural) return dot_matrix_t;
-	function col(x : natural; dots: dot_matrix_t) return std_logic_vector;
-	function row(y : natural; dots: dot_matrix_t) return std_logic_vector;
-	function get_char_line(dots:		dot_matrix_t;
-			       line_nr:		natural;
-			       module_sel:	std_logic) return std_logic_vector;
 		when 0  => dot_char := (('0','0','0','0','0'),
 					('0','0','0','0','0'),
 					('0','0','0','0','0'),
@@ -200,6 +163,10 @@ end package body dot_fonts;
 					    ('1','1','1','1','1'),
 					    ('1','1','1','1','1'),
 					    ('1','1','1','1','1'));
+	end case;
+	return dot_char;
+	end function get_dot_char;
+
 	-- extracts a column from the given dot matrix
 	function col(x : natural; dots : dot_matrix_t) return std_logic_vector is
 	begin
@@ -237,3 +204,5 @@ end package body dot_fonts;
 		end if;
 		return dl;
 	end function get_char_line;
+
+end package body dot_fonts;
